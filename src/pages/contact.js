7284f31ec5c5ea7e49fbc701e4ebf12d95c12dfc
Donaël Walter <w.dodo.a@hotmail.fr>
2019-05-25 22:58:49 +0200

    add content in pages

---
+++ src/pages/contact.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Layout from "../components/Layout"
+//import UncontrolledLottie from "../components/UncontrolledLottie"
+//import ControlledLottie from "../components/ControlledLottie"
+
+export default () => (
+  <div>
+    <Layout>
+      <div>
+        <p>Contact</p>
+      </div>
+    </Layout>
+  </div>
+)
